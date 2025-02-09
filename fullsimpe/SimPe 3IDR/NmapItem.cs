a3375a0667c6b56e6d89dc8c4d670ce6c0a241ae
autinerd <27780930+autinerd@users.noreply.github.com>
2025-02-09 20:36:56 +0100

    Update SimPe 3IDR

--- fullsimpe/SimPe 3IDR/NmapItem.cs
+++ fullsimpe/SimPe 3IDR/NmapItem.cs
@@ -22,7 +22,7 @@ using System;
 namespace SimPe.Plugin
 {
 	/// <summary>
-	/// Zusammenfassung für PackedFileItem.
+	/// Summary description for PackedFileItem.
 	/// </summary>
 	public class NmapItem : SimPe.Packages.PackedFileDescriptor
 	{
