caf89fa822763f10dbdd6ee58425a6543ff92a7e
autinerd <27780930+autinerd@users.noreply.github.com>
2025-02-23 14:22:51 +0100

    Update Wizards of SimPe

--- fullsimpe/Wizards of SimPe/Form1.cs
+++ fullsimpe/Wizards of SimPe/Form1.cs
@@ -27,7 +27,7 @@ using System.Data;
 namespace SimPe.Wizards
 {
 	/// <summary>
-	/// Zusammenfassung für Form1.
+	/// Summary description for Form1.
 	/// </summary>
 	public class Form1 : System.Windows.Forms.Form
 	{
@@ -38,11 +38,10 @@ namespace SimPe.Wizards
 		private System.Windows.Forms.Label lbstep;
 		private System.Windows.Forms.LinkLabel llnext;
 		private System.Windows.Forms.LinkLabel llback;
-		private System.Windows.Forms.Panel panel2;
 		private System.Windows.Forms.LinkLabel llopt;
 		internal System.Windows.Forms.Panel pnP;
 		/// <summary>
-		/// Erforderliche Designervariable.
+		/// Required designer variable.
 		/// </summary>
 		private System.ComponentModel.Container components = null;
 		private System.Windows.Forms.Label lbmsg;
@@ -54,36 +53,30 @@ namespace SimPe.Wizards
 		public Form1()
 		{
 			//
-			// Erforderlich für die Windows Form-Designerunterstützung
+			// Required designer variable.
 			//
 			InitializeComponent();
-#if MAC
-#else
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-#endif
 			this.lbstep.ForeColor = Color.FromArgb(0x40, this.lbstep.ForeColor);
 			this.lbmsg.ForeColor = Color.FromArgb(0xb0, this.lbmsg.ForeColor);
 
 			step1 = new FormStep1();
 			prevsteps.Push(step1);
 			ShowStep(step1, true);
-
-#if MAC
-#else
 			if ((!Option.HaveObjects) || (!Option.HaveSavefolder))
 			{
-				MessageBox.Show("Your Path settings are invalid. Wizards of SimPE will direct you to the Options Page.\n\nYou can just click on the 'Suggest' Buttons there, to get the default Paths. If the 'Suggest' Button disapears, your Path is set correct.", "Warning", MessageBoxButtons.OK);
+				MessageBox.Show("Your Path settings are invalid. Wizards of SimPe will direct you to the Options Page.\n\nYou can just click on the 'Suggest' Buttons there, to get the default Paths. If the 'Suggest' Button disapears, your Path is set correct.", "Warning", MessageBoxButtons.OK);
 				this.ShowOptions(null, null);
 			}
-#endif
 
 			Wait.Bar = new SimPe.Wizards.WaitBarControl(this);	
 			if (SimPe.FileTable.FileIndex==null) SimPe.FileTable.FileIndex = new SimPe.Plugin.FileIndex();
 			SimPe.Packages.PackageMaintainer.Maintainer.FileIndex = SimPe.FileTable.FileIndex;
+			if (Helper.WindowsRegistry.UseBigIcons) this.pndrop.Font = new System.Drawing.Font("Tahoma", 12F);
 		}
 
 		/// <summary>
-		/// Die verwendeten Ressourcen bereinigen.
+		/// Clean up any resources being used.
 		/// </summary>
 		protected override void Dispose( bool disposing )
 		{
@@ -97,19 +90,18 @@ namespace SimPe.Wizards
 			base.Dispose( disposing );
 		}
 
-		#region Vom Windows Form-Designer generierter Code
+		#region Windows Form Designer generated code
 		/// <summary>
-		/// Erforderliche Methode für die Designerunterstützung. 
-		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(Form1));
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.pbtop = new System.Windows.Forms.PictureBox();
 			this.pbbottom = new System.Windows.Forms.PictureBox();
 			this.pndrop = new System.Windows.Forms.Panel();
 			this.llopt = new System.Windows.Forms.LinkLabel();
-			this.panel2 = new System.Windows.Forms.Panel();
 			this.llback = new System.Windows.Forms.LinkLabel();
 			this.llnext = new System.Windows.Forms.LinkLabel();
 			this.pbstretch = new System.Windows.Forms.PictureBox();
@@ -118,17 +110,20 @@ namespace SimPe.Wizards
 			this.pbP = new Ambertation.Windows.Forms.ExtProgressBar();
 			this.lbPmsg = new System.Windows.Forms.Label();
 			this.lbmsg = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pbtop)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbbottom)).BeginInit();
 			this.pndrop.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbstretch)).BeginInit();
 			this.pnP.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// pbtop
 			// 
-			this.pbtop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbtop.BackgroundImage")));
 			this.pbtop.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pbtop.Image = ((System.Drawing.Image)(resources.GetObject("pbtop.Image")));
 			this.pbtop.Location = new System.Drawing.Point(0, 0);
 			this.pbtop.Name = "pbtop";
-			this.pbtop.Size = new System.Drawing.Size(612, 153);
+			this.pbtop.Size = new System.Drawing.Size(1032, 153);
 			this.pbtop.TabIndex = 0;
 			this.pbtop.TabStop = false;
 			// 
@@ -136,23 +131,22 @@ namespace SimPe.Wizards
 			// 
 			this.pbbottom.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbbottom.BackgroundImage")));
 			this.pbbottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.pbbottom.Location = new System.Drawing.Point(0, 480);
+			this.pbbottom.Location = new System.Drawing.Point(0, 612);
 			this.pbbottom.Name = "pbbottom";
-			this.pbbottom.Size = new System.Drawing.Size(612, 24);
+			this.pbbottom.Size = new System.Drawing.Size(1032, 24);
 			this.pbbottom.TabIndex = 1;
 			this.pbbottom.TabStop = false;
 			// 
 			// pndrop
 			// 
 			this.pndrop.Controls.Add(this.llopt);
-			this.pndrop.Controls.Add(this.panel2);
 			this.pndrop.Controls.Add(this.llback);
 			this.pndrop.Controls.Add(this.llnext);
 			this.pndrop.Controls.Add(this.pbstretch);
 			this.pndrop.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.pndrop.Location = new System.Drawing.Point(0, 153);
 			this.pndrop.Name = "pndrop";
-			this.pndrop.Size = new System.Drawing.Size(612, 327);
+			this.pndrop.Size = new System.Drawing.Size(1032, 459);
 			this.pndrop.TabIndex = 4;
 			// 
 			// llopt
@@ -160,25 +154,16 @@ namespace SimPe.Wizards
 			this.llopt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.llopt.AutoSize = true;
 			this.llopt.BackColor = System.Drawing.Color.White;
-			this.llopt.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.llopt.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.llopt.LinkColor = System.Drawing.Color.Red;
-			this.llopt.Location = new System.Drawing.Point(24, 312);
+			this.llopt.Location = new System.Drawing.Point(24, 444);
 			this.llopt.Name = "llopt";
-			this.llopt.Size = new System.Drawing.Size(63, 19);
+			this.llopt.Size = new System.Drawing.Size(64, 16);
 			this.llopt.TabIndex = 16;
 			this.llopt.TabStop = true;
 			this.llopt.Text = "Options";
 			this.llopt.VisitedLinkColor = System.Drawing.Color.Maroon;
 			this.llopt.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.ShowOptions);
-			// 
-			// panel2
-			// 
-			this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.panel2.BackColor = System.Drawing.Color.LightGray;
-			this.panel2.Location = new System.Drawing.Point(338, 309);
-			this.panel2.Name = "panel2";
-			this.panel2.Size = new System.Drawing.Size(232, 1);
-			this.panel2.TabIndex = 15;
 			// 
 			// llback
 			// 
@@ -186,11 +171,11 @@ namespace SimPe.Wizards
 			this.llback.AutoSize = true;
 			this.llback.BackColor = System.Drawing.Color.White;
 			this.llback.Enabled = false;
-			this.llback.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.llback.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.llback.LinkColor = System.Drawing.Color.Red;
-			this.llback.Location = new System.Drawing.Point(458, 311);
+			this.llback.Location = new System.Drawing.Point(858, 443);
 			this.llback.Name = "llback";
-			this.llback.Size = new System.Drawing.Size(57, 19);
+			this.llback.Size = new System.Drawing.Size(68, 18);
 			this.llback.TabIndex = 13;
 			this.llback.TabStop = true;
 			this.llback.Text = "< Back";
@@ -203,11 +188,11 @@ namespace SimPe.Wizards
 			this.llnext.AutoSize = true;
 			this.llnext.BackColor = System.Drawing.Color.White;
 			this.llnext.Enabled = false;
-			this.llnext.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.llnext.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.llnext.LinkColor = System.Drawing.Color.Red;
-			this.llnext.Location = new System.Drawing.Point(522, 311);
+			this.llnext.Location = new System.Drawing.Point(942, 443);
 			this.llnext.Name = "llnext";
-			this.llnext.Size = new System.Drawing.Size(56, 19);
+			this.llnext.Size = new System.Drawing.Size(68, 18);
 			this.llnext.TabIndex = 12;
 			this.llnext.TabStop = true;
 			this.llnext.Text = "Next >";
@@ -220,7 +205,7 @@ namespace SimPe.Wizards
 			this.pbstretch.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.pbstretch.Location = new System.Drawing.Point(0, 0);
 			this.pbstretch.Name = "pbstretch";
-			this.pbstretch.Size = new System.Drawing.Size(612, 327);
+			this.pbstretch.Size = new System.Drawing.Size(1032, 459);
 			this.pbstretch.TabIndex = 5;
 			this.pbstretch.TabStop = false;
 			// 
@@ -228,30 +213,31 @@ namespace SimPe.Wizards
 			// 
 			this.lbstep.AutoSize = true;
 			this.lbstep.BackColor = System.Drawing.Color.Transparent;
-			this.lbstep.Font = new System.Drawing.Font("Georgia", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.lbstep.Font = new System.Drawing.Font("Georgia", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lbstep.ForeColor = System.Drawing.Color.White;
-			this.lbstep.Location = new System.Drawing.Point(512, 4);
+			this.lbstep.Location = new System.Drawing.Point(922, 0);
 			this.lbstep.Name = "lbstep";
-			this.lbstep.Size = new System.Drawing.Size(102, 113);
+			this.lbstep.Size = new System.Drawing.Size(98, 96);
 			this.lbstep.TabIndex = 10;
 			this.lbstep.Text = "0";
 			this.lbstep.TextAlign = System.Drawing.ContentAlignment.BottomRight;
 			// 
 			// pnP
 			// 
+			this.pnP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.pnP.BackColor = System.Drawing.Color.White;
 			this.pnP.Controls.Add(this.pbP);
 			this.pnP.Controls.Add(this.lbPmsg);
-			this.pnP.Location = new System.Drawing.Point(24, 464);
+			this.pnP.Location = new System.Drawing.Point(24, 596);
 			this.pnP.Name = "pnP";
-			this.pnP.Size = new System.Drawing.Size(384, 24);
+			this.pnP.Size = new System.Drawing.Size(634, 24);
 			this.pnP.TabIndex = 5;
 			this.pnP.Visible = false;
 			// 
 			// pbP
 			// 
 			this.pbP.BackColor = System.Drawing.Color.Transparent;
-			this.pbP.BorderColor = System.Drawing.Color.FromArgb(((System.Byte)(100)), ((System.Byte)(0)), ((System.Byte)(0)), ((System.Byte)(0)));
+			this.pbP.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
 			this.pbP.Gradient = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
 			this.pbP.GradientEndColor = System.Drawing.Color.White;
 			this.pbP.GradientStartColor = System.Drawing.Color.White;
@@ -262,20 +248,21 @@ namespace SimPe.Wizards
 			this.pbP.ProgressBackColor = System.Drawing.SystemColors.Window;
 			this.pbP.Quality = true;
 			this.pbP.SelectedColor = System.Drawing.Color.Orange;
-			this.pbP.Size = new System.Drawing.Size(128, 16);
-			this.pbP.Style = Ambertation.Windows.Forms.ProgresBarStyle.Flat;
+			this.pbP.Size = new System.Drawing.Size(372, 16);
+			this.pbP.Style = booby.ProgresBarStyle.Flat;
 			this.pbP.TabIndex = 16;
-			this.pbP.TokenCount = 10;
+			this.pbP.TokenCount = 20;
 			this.pbP.UnselectedColor = System.Drawing.Color.Gray;
+			this.pbP.UseTokenBuffer = true;
 			this.pbP.Value = 50;
 			// 
 			// lbPmsg
 			// 
-			this.lbPmsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right)));
-			this.lbPmsg.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.lbPmsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.lbPmsg.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lbPmsg.ForeColor = System.Drawing.Color.Silver;
-			this.lbPmsg.Location = new System.Drawing.Point(128, 0);
+			this.lbPmsg.Location = new System.Drawing.Point(378, 0);
 			this.lbPmsg.Name = "lbPmsg";
 			this.lbPmsg.Size = new System.Drawing.Size(256, 24);
 			this.lbPmsg.TabIndex = 15;
@@ -285,21 +272,22 @@ namespace SimPe.Wizards
 			// lbmsg
 			// 
 			this.lbmsg.BackColor = System.Drawing.Color.Transparent;
-			this.lbmsg.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
+			this.lbmsg.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.lbmsg.ForeColor = System.Drawing.Color.DimGray;
-			this.lbmsg.Location = new System.Drawing.Point(240, 16);
+			this.lbmsg.Location = new System.Drawing.Point(248, 14);
 			this.lbmsg.Name = "lbmsg";
-			this.lbmsg.Size = new System.Drawing.Size(280, 72);
+			this.lbmsg.Size = new System.Drawing.Size(598, 72);
 			this.lbmsg.TabIndex = 6;
 			this.lbmsg.Text = "Description";
 			this.lbmsg.TextAlign = System.Drawing.ContentAlignment.BottomRight;
 			// 
 			// Form1
 			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.BackColor = System.Drawing.Color.FromArgb(((System.Byte)(102)), ((System.Byte)(102)), ((System.Byte)(153)));
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(153)))));
 			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-			this.ClientSize = new System.Drawing.Size(612, 504);
+			this.ClientSize = new System.Drawing.Size(1032, 636);
 			this.Controls.Add(this.pnP);
 			this.Controls.Add(this.pndrop);
 			this.Controls.Add(this.pbbottom);
@@ -309,17 +297,20 @@ namespace SimPe.Wizards
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MaximizeBox = false;
-			this.MaximumSize = new System.Drawing.Size(618, 800);
-			this.MinimumSize = new System.Drawing.Size(618, 216);
 			this.Name = "Form1";
-
-
-			this.Text = "Wizards of SimPE";
-			this.Closing += new System.ComponentModel.CancelEventHandler(this.Close);
+			this.Opacity = 0.94;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Wizards of SimPe";
 			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Closing += new System.ComponentModel.CancelEventHandler(this.Close);
+			((System.ComponentModel.ISupportInitialize)(this.pbtop)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbbottom)).EndInit();
 			this.pndrop.ResumeLayout(false);
+			this.pndrop.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbstretch)).EndInit();
 			this.pnP.ResumeLayout(false);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 		#endregion
@@ -327,7 +318,7 @@ namespace SimPe.Wizards
 		internal static Form1 form1;
 
 		/// <summary>
-		/// Der Haupteinstiegspunkt für die Anwendung.
+		/// Der Haupteinstiegspunkt f�r die Anwendung.
 		/// </summary>
 		[STAThread]			
 		static void Main() 
@@ -340,12 +331,8 @@ namespace SimPe.Wizards
 				SimPe.Helper.WindowsRegistry.HiddenMode = false;
 				SimPe.Helper.WindowsRegistry.AsynchronLoad = false;
 				SimPe.Plugin.ScenegraphWrapperFactory.InitRcolBlocks();
-
-#if MAC
-				Console.WriteLine("GameDir: "+Helper.WindowsRegistry.SimsPath);
-				Console.WriteLine("SaveDir: "+Helper.WindowsRegistry.SimSavegameFolder);
-#endif
 				form1 = new Form1();
+				Application.EnableVisualStyles();
 				Application.Run(form1);
 
 				SimPe.Helper.WindowsRegistry.HiddenMode = adv;
@@ -356,11 +343,6 @@ namespace SimPe.Wizards
 			catch (Exception ex)
 			{
 				MessageBox.Show("WOS will Shutdown due to an unhandled Exception. \n\nMessage:"+ex.Message);
-#if MAC
-				Console.WriteLine(ex.Message);
-				Console.WriteLine("Source: "+ex.Source);
-				Console.WriteLine("Stack: "+ex.StackTrace);
-#endif
 			}
 		}
 
@@ -436,22 +418,16 @@ namespace SimPe.Wizards
 
 		Option op = new Option();
 		private void ShowOptions(object sender, System.Windows.Forms.LinkLabelLinkClickedEventArgs e)
-		{						
+		{
+			this.llopt.LinkVisited = true;
 			op.form1 = this;
 			op.Location = pndrop.Location;
 			op.Size = pndrop.Size;
 			this.Controls.Add(op.pnopt);
 			op.pnopt.Parent = this;
-
-#if MAC
-			Console.WriteLine(pndrop.Location);
-			Console.WriteLine(pndrop.Size);
-#else
-            op.tbsims.Text = PathProvider.Global[Expansions.BaseGame].InstallFolder;
-            op.tbsave.Text = PathProvider.SimSavegameFolder;
-            op.tbdds.Text = PathProvider.Global.NvidiaDDSPath;
-#endif
-
+			op.tbsims.Text = PathProvider.Global[Expansions.BaseGame].InstallFolder;
+			op.tbsave.Text = PathProvider.SimSavegameFolder;
+			op.tbdds.Text = PathProvider.Global.NvidiaDDSPath;
 			op.pnopt.Visible = true;
 			pndrop.Visible = false;
 		}
