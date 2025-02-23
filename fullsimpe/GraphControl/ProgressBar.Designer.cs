2e0bdbb8b8763a4ce03ad46afd1376a6e4f6055e
autinerd <27780930+autinerd@users.noreply.github.com>
2025-02-23 16:09:24 +0100

    Fix GraphControl

---
+++ fullsimpe/GraphControl/ProgressBar.Designer.cs
@@ -0,0 +1,45 @@
+namespace Ambertation.Windows.Forms
+{
+    partial class ProgressBar
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelÃ¶scht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode fÃ¼r die DesignerunterstÃ¼tzung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geÃ¤ndert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ProgressBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ProgressBar";
+            this.Size = new System.Drawing.Size(294, 16);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
