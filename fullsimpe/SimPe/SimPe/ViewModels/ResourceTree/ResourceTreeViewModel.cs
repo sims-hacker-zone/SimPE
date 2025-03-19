49284e43037891df745aca8b7302608e279a3543
autinerd <27780930+autinerd@users.noreply.github.com>
2025-03-19 18:47:31 +0100

    Show all resources in data grid when "All Resources" is selected

--- fullsimpe/SimPe/SimPe/ViewModels/ResourceTree/ResourceTreeViewModel.cs
+++ fullsimpe/SimPe/SimPe/ViewModels/ResourceTree/ResourceTreeViewModel.cs
@@ -5,7 +5,11 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 
+using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
+
 using SimPe.Models.Package;
+using SimPe.Models.PackedFile;
 
 namespace SimPe.ViewModels.ResourceTree
 {
@@ -21,6 +25,11 @@ namespace SimPe.ViewModels.ResourceTree
 			get; set;
 		}
 
+		public FlatTreeDataGridSource<PackedFile> FileSource
+		{
+			get; set;
+		}
+
 		public string Caption => $"All Resources ({packageFile.FileIndex.Count})";
 
 		public ResourceTreeViewModel(PackageFile packageFile, MainWindowViewModel parent)
@@ -28,6 +37,17 @@ namespace SimPe.ViewModels.ResourceTree
 			this.packageFile = packageFile;
 			this.parent = parent;
 			Nodes = new(packageFile.FileIndex.GroupBy(item => item.Type).Select(item => new ResourceTreeTypeViewModel(packageFile, item.Key, this)).OrderBy(item => item.Caption));
+			FileSource = new(packageFile.FileIndex);
+			FileSource.Columns.AddRange([
+				new TextColumn<PackedFile, string>("Name", x => x.DisplayName),
+				new TextColumn<PackedFile, string>("Type", x => x.TypeInfo.ShortName),
+				new TextColumn<PackedFile, string>("Group", x => $"0x{x.Group:X8}"),
+				new TextColumn<PackedFile, string>("Instance (high)", x => $"0x{x.InstanceHigh:X8}"),
+				new TextColumn<PackedFile, string>("Instance", x => $"0x{x.Instance:X8}"),
+				new TextColumn<PackedFile, int>("Size", x => x.Size),
+				new TextColumn<PackedFile, string>("Uncompressed size", x => $"{(x.UncompressedSize == 0 ? "" : x.UncompressedSize)}"),
+			]);
+			FileSource.RowSelection.SelectionChanged += parent.Parent.RowSelection_Changed;
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Caption)));
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nodes)));
 		}
